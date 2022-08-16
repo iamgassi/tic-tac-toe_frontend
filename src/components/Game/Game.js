eb4026af14902768edcff5ac7e4f1c7bb78ad84e
Gaurav Gassi <gauravgassi123@gmail.com>
2022-08-16 14:42:10 +0530

    changes

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,11 +1,120 @@
-import React from 'react'
+import React, { useState } from 'react';
+import style from './Game.module.css';
 
 const Game = () => {
-  return (
-    <div>
-      Tic tac toe
-    </div>
-  )
-}
+	const [turn, setTurn] = useState('x');
+	const [cells, setCells] = useState(Array(9).fill(''));
+	const [winner, setWinner] = useState();
+  
 
-export default Game
+	const checkForWinner = (squares) => {
+		let combos = {
+			across: [
+				[0, 1, 2],
+				[3, 4, 5],
+				[6, 7, 8],
+			],
+			down: [
+				[0, 3, 6],
+				[1, 4, 7],
+				[2, 5, 8],
+			],
+			diagnol: [
+				[0, 4, 8],
+				[2, 4, 6],
+			],
+		};
+
+		for (let combo in combos) {
+			combos[combo].forEach((pattern) => {
+				if (
+					squares[pattern[0]] === '' ||
+					squares[pattern[1]] === '' ||
+					squares[pattern[2]] === ''
+				) {
+					// do nothing
+				} else if (
+					squares[pattern[0]] === squares[pattern[1]] &&
+					squares[pattern[1]] === squares[pattern[2]]
+				) {
+					setWinner(squares[pattern[0]]);
+				}
+			});
+		}
+	};
+
+	const handleClick = (num) => {
+    if(winner)
+    return ;
+		if (cells[num] !== '') {
+			alert('already clicked');
+			return;
+		}
+
+		let squares = [...cells];
+
+		if (turn === 'x') {
+
+			squares[num] = 'x';
+			setTurn('o');
+		} else {
+			squares[num] = 'o';
+			setTurn('x');
+		}
+
+		checkForWinner(squares);
+		setCells(squares);
+	};
+  
+	const handleRestart = () => {
+		setWinner(null);
+		setCells(Array(9).fill(''));
+	};
+
+	const Cell = ({ num }) => {
+		return <td onClick={() => handleClick(num)}>{cells[num]}</td>;
+	};
+
+	return (
+		<div className={style.container}>
+			<table>
+				Turn: {turn}
+				<tbody>
+					<tr>
+						<Cell num={0} />
+						<Cell num={1} />
+						<Cell num={2} />
+					</tr>
+					<tr>
+						<Cell num={3} />
+						<Cell num={4} />
+						<Cell num={5} />
+					</tr>
+					<tr>
+						<Cell num={6} />
+						<Cell num={7} />
+						<Cell num={8} />
+					</tr>
+				</tbody>
+			</table>
+			{winner ?(
+				<>
+					<p>{winner} is the winner!</p>
+					<button onClick={() => handleRestart()}>Play Again</button>
+          
+				</>
+			):
+      ( (!cells.includes(''))?(
+        <>
+					<p> PLAY AGAIN, MATCH IS DRAWN! </p>
+					<button onClick={() => handleRestart()}>Play Again</button>
+				</>
+        ):(null)
+        
+      )}
+		</div>
+	);
+};
+
+export default Game;
+
